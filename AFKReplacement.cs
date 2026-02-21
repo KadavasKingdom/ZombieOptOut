ae3e07e3f901c718ea6510977f041c181d41f440
Kadava <kadavamail@gmail.com>
2026-02-21 03:02:19 +0000

    Custom role fix

--- AFKReplacement.cs
+++ AFKReplacement.cs
@@ -12,7 +12,8 @@ public class AFKReplacement
     public static float health = 0;
     public static Dictionary<RoleTypeId, CustomRoleBaseInfo> cachedCustomRole = new Dictionary<RoleTypeId, CustomRoleBaseInfo>();
     public static bool canReplace = false;
-    private static PlayerChangingRoleEventArgs cahcedArgs;
+    private static PlayerChangingRoleEventArgs cachedArgs;
+    private static CoroutineHandle fillTimerCoroutine;
 
     public static void OnServerRoundStarted()
     {
@@ -27,28 +28,34 @@ public class AFKReplacement
     }
 
     // ADD DUMMY DETECTION
-    //Caching information before disconnect
+    //Caching information before disconnect or when a main SCP spawns
     public static void OnRoleChanging(PlayerChangingRoleEventArgs ev)
     {
-        cahcedArgs = ev;
-        CL.Info($"[OnRoleChanging] Player changing role event triggered for {cahcedArgs.Player.Nickname} | New Role: {cahcedArgs.NewRole} | Old Role: {cahcedArgs.OldRole.RoleTypeId} | Health: {cahcedArgs.Player.Health}");
+        cachedArgs = ev;
+        CL.Info($"[OnRoleChanging] Player changing role event triggered for {cachedArgs.Player.Nickname} | New Role: {cachedArgs.NewRole} | Old Role: {cachedArgs.OldRole.RoleTypeId} | Health: {cachedArgs.Player.Health}");
 
         if (!withinRoundStart)
             return;
 
-        if (cahcedArgs.NewRole == RoleTypeId.Spectator && cahcedArgs.OldRole.RoleTypeId.IsScp() && cahcedArgs.OldRole.RoleTypeId != RoleTypeId.Scp0492)
+        //Caches custom role when it initially spawns (Anything -> SCP)
+        if(cachedArgs.NewRole.IsScp() && cachedArgs.NewRole != RoleTypeId.Scp0492)
         {
-            health = cahcedArgs.Player.Health;
-
-            //Custom role handling
             CustomRoleBaseInfo savedCustomRole = null;
 
-            if (SimpleCustomRoles.Helpers.CustomRoleHelpers.TryGetCustomRole(cahcedArgs.Player, out savedCustomRole))
-                cachedCustomRole.Add(cahcedArgs.OldRole.RoleTypeId, savedCustomRole);
+            if (SimpleCustomRoles.Helpers.CustomRoleHelpers.TryGetCustomRole(cachedArgs.Player, out savedCustomRole))
+                cachedCustomRole.Add(cachedArgs.OldRole.RoleTypeId, savedCustomRole);
             else
-                cachedCustomRole.Add(cahcedArgs.OldRole.RoleTypeId, null);
+                cachedCustomRole.Add(cachedArgs.OldRole.RoleTypeId, null);
 
-            CL.Info($"Custom role: {savedCustomRole.Rolename}");
+            CL.Info($"Custom role cached: {savedCustomRole.Rolename}");
+        }
+
+        //Runs when a player disconnects or dies (SCP -> Spectator)
+        if (cachedArgs.NewRole == RoleTypeId.Spectator && cachedArgs.OldRole.RoleTypeId.IsScp() && cachedArgs.OldRole.RoleTypeId != RoleTypeId.Scp0492)
+        {
+            health = cachedArgs.Player.Health;
+            canReplace = true;
+            CL.Info($"Health cached: {health}");
         }
     }
 
@@ -69,7 +76,6 @@ public class AFKReplacement
             if (!withinRoundStart)
                 return;
 
-            canReplace = true;
 
             foreach (Player player in Player.ReadyList)
             {
@@ -85,8 +91,15 @@ public class AFKReplacement
                     continue;
 
                 //TODO Detect if its a custom role and alter message
-                player.SendBroadcast($"<size=40>[AFK Replacement] <b>{cachedCustomRole.LastOrDefault().Key} ({cachedCustomRole.LastOrDefault().Value} | {health}hp)</b> has disconnected!\n</size><size=34>You can take their spot by typing <b>.fill</b> in your console (`)!</size>", 5);
+                CL.Info($"<size=40>[AFK Replacement] <b>{cachedCustomRole.LastOrDefault().Key} ({cachedCustomRole.LastOrDefault().Value.Rolename} | {health}hp)</b> has disconnected!\n</size><size=34>You can take their spot by typing <b>.fill</b> in your console (`)!</size>");
+                player.ClearBroadcasts();
+                player.SendBroadcast($"<size=40>[AFK Replacement] <b>{cachedCustomRole.LastOrDefault().Key} ({cachedCustomRole.LastOrDefault().Value.Rolename} | {health}hp)</b> has disconnected!\n</size><size=34>You can take their spot by typing <b>.fill</b> in your console (`)!</size>", 5);
             }
+
+            if(fillTimerCoroutine != null || !fillTimerCoroutine.IsValid)
+                Timing.KillCoroutines(fillTimerCoroutine);
+
+            fillTimerCoroutine = Timing.RunCoroutine(fillTimeout());
         });
     }
 
@@ -97,8 +110,21 @@ public class AFKReplacement
         if (cachedCustomRole.LastOrDefault().Value != null)
             Timing.CallDelayed(0.5f, () => Server.RunCommand($"/scr set {cachedCustomRole.LastOrDefault().Value.Rolename} {fillingPlayer.PlayerId}"));
 
+        Server.ClearBroadcasts();
+        Server.SendBroadcast($"[AFK Replacement] {cachedCustomRole.LastOrDefault().Key} has been replaced!", 5);
+
         Timing.CallDelayed(2.5f, () => fillingPlayer.Health = health);
 
+        if (fillTimerCoroutine != null || !fillTimerCoroutine.IsValid)
+            Timing.KillCoroutines(fillTimerCoroutine);
+
+        cachedCustomRole.Clear();
+        canReplace = false;
+    }
+
+    private static IEnumerator<float> fillTimeout()
+    {
+        yield return Timing.WaitForSeconds(15f);
         cachedCustomRole.Clear();
         canReplace = false;
     }
