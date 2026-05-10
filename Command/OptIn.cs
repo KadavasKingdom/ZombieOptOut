dd0127e64e3bffc6a2955c4d8298e39bcd2c920d
z5tm <onthebehalfofz5tm@gmail.com>
2026-05-10 01:21:20 -0500

    refactor(opt-in): proper PascalCase, player null checking, introduce NeedReplacement variable

--- Command/OptIn.cs
+++ Command/OptIn.cs
@@ -4,31 +4,27 @@ using RemoteAdmin;
 namespace ZombieOptOut.Command;
 
 [CommandHandler(typeof(ClientCommandHandler))]
-[CommandHandler(typeof(GameConsoleCommandHandler))]
-public class optin : ICommand
+public class OptIn : ICommand
 {
     public string Command => "ZombieOptIn";
     public string[] Aliases => ["optin", "zombieoptin", "opt", "oi"];
     public string Description => "Replace an opted out zombie player";
-    public bool SanitizeResponse => true;
 
     public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
     {
-        response = null;
-
-        if (sender is not PlayerCommandSender)
+        var player = Player.Get(sender);
+        if (sender is not PlayerCommandSender || player == null)
         {
             response = "This command can only be ran by a player!";
             return false;
         }
 
-        if (OptOutSystem.waitingForCompensationFrom == 0)
+        if (!OptOutSystem.NeedReplacement)
         {
             response = "There's no zombie roles waiting to be filled! You can enable Auto-Fill in the settings to never miss out!";
             return false;
         }
 
-        Player player = Player.Get(sender);
 
         OptOutSystem.RoleFill(player);
         response = "You've filled in for a missing zombie!";
