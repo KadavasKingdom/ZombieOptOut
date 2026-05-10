3b96f435b63c3363c0bd61f9bf65bb977f158dbe
z5tm <onthebehalfofz5tm@gmail.com>
2026-05-10 01:25:13 -0500

    feat(config): more configurable properties

--- Config.cs
+++ Config.cs
@@ -9,4 +9,7 @@ public sealed class Config
     public float ZombieFillDuration { get; set; } = 10f;
     public float HealthCompensation { get; set; } = 200f;
     public bool DisableXPLoss { get; set; } = true;
+    public int OptInExp { get; set; } = 100;
+    public bool StackZombieCompensation { get; set; } = true;
+    public bool UseCustomRoles { get; set; } = true;
 }
