3702fbb52a26c05a0962c3c61fe721393b0c8b20
z5tm <onthebehalfofz5tm@gmail.com>
2026-05-10 01:24:40 -0500

    refactor(afk-replacement-patch): proper PascalCase, remove unused usings

--- Patches/AfkReplacementPatch.cs
+++ Patches/AfkReplacementPatch.cs
@@ -1,20 +1,12 @@
-﻿using CustomPlayerEffects;
-using HarmonyLib;
+﻿using HarmonyLib;
 using Mirror;
-using PlayerRoles;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ZombieOptOut.Patches;
-
 
 // this simple patch just moves the logic for handling disconnected players to before they take damage
 // this is as to cache their health before they died
 [HarmonyPatch(typeof(CustomNetworkManager), nameof(CustomNetworkManager.OnServerDisconnect))]
-internal class CustomNetworkManager_OnServerDisconnect
+internal class AfkReplacementPatch
 {
     public static void Prefix(CustomNetworkManager __instance, NetworkConnectionToClient conn)
     {
