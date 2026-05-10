98a95a71fb1b57974f81a97a6859fef731975128
z5tm <onthebehalfofz5tm@gmail.com>
2026-05-10 01:20:01 -0500

    refactor(opt-out): remove fragile Timing.Wait calls, use event-based role setting.
    Side effect: Removes the necessity for setting custom roles, as custom roles are only set on zombie spawn, rather than spawning in a zombie first to cache the custom role.

--- OptoutSystem.cs
+++ OptoutSystem.cs
@@ -1,73 +1,101 @@
-﻿using LabApi.Events.Arguments.Scp049Events;
+﻿using LabApi.Events.Arguments.PlayerEvents;
+using LabApi.Events.Arguments.Scp049Events;
 using MEC;
 using PlayerRoles;
-using SimpleCustomRoles.RoleYaml;
+using SimpleCustomRoles.Helpers;
 using UnityEngine;
 
 namespace ZombieOptOut;
 
-public class OptOutSystem
+public static class OptOutSystem
 {
-    public static uint waitingForCompensationFrom = 0;
-    private static Player optedOutPlayer = null;
-    private static Player main049Player = null;
-    private static CustomRoleBaseInfo savedCustomRole = null;
-
-    public static void RevivedZombie(Scp049ResurrectedBodyEventArgs ev)
+    public static bool NeedReplacement => _optedOutZombies.Count != 0;
+    private static string? _latest049Player; // latest 049 player's userId
+    private static Vector3? GetReadyPosition()
     {
-        main049Player = ev.Player;
-        ReferenceHub refHub = ev.Target.ReferenceHub;
+        var latest049Player = Player.Get(_latest049Player);
+        
+        if (latest049Player is { IsAlive: true })
+            return latest049Player.Position;
+        
+        _latest049Player = null;
+        return null;
+    }
+    /// <key name="string">UserId</key>
+    /// <value name="Vector3">Saved death position</value>
+    private static HashSet<string> _optedOutZombies = [];
+    private static Dictionary<string, Vector3> _positionsToReplace = []; // Cached positions to replace after 
 
-        if (ServerSpecificSettings.savedSettings[refHub].Item1 == false)
+    public static void RevivingZombie(Scp049ResurrectingBodyEventArgs ev)
+    {
+        if (_optedOutZombies.Contains(ev.Player.UserId))
             return;
+        
+        var playerAndRoles = CustomRoleHelpers.GetPlayerAndRoles();
+        if ((Main.Instance.Config?.UseCustomRoles ?? Defaults.UseCustomRoles) && playerAndRoles.TryGetValue(ev.Player, out var customRole) && customRole.Rolename.Contains("035"))
+            return;
+        
+        _latest049Player = ev.Player.UserId;
+        var refHub = ev.Target.ReferenceHub;
+        
+        if (!ServerSpecificSettings.GetOptOutEnabled(refHub)) // item1 is optOut, item2 is autoFill
+            return;
+        
+        _optedOutZombies.Add(ev.Target.UserId); // NOTE: You can use ev.Ragdoll.Position here, but offset it by a bit when you do.
+        
+        ev.Target.SendBroadcast($"<size=36>[ZombieOptOut] You've opted out of being revived as a zombie in your Settings!</size>", 5);
+        ev.IsAllowed = false;
+        
+        var optedOutPlayer = ev.Target;
 
-        Timing.CallDelayed(1f, () =>
-        {
-            if (SimpleCustomRoles.Helpers.CustomRoleHelpers.Contains(ev.Target))
-            {
-                SimpleCustomRoles.Helpers.CustomRoleHelpers.GetPlayerAndRoles().TryGetValue(ev.Target, out savedCustomRole);
-                CL.Info("Custom role saved: " + savedCustomRole.Rolename);
-            }
-
-            ev.Target.SendBroadcast($"<size=36>[ZombieOptOut] You've opted out of being revived as a zombie in your Settings!</size>", 5);
-            if (ev.Target.Role == RoleTypeId.Scp0492)
-                ev.Target.SetRole(RoleTypeId.Spectator);
-        });
-
-
-        optedOutPlayer = ev.Target;
-
-        foreach (Player player in Player.ReadyList)
+        foreach (var player in Player.ReadyList.ToArray())
         {
             if (player == ev.Target)
                 continue;
-
-            if (player.IsDummy)
+            
+            if (player is {IsAlive: true} or {IsDummy: true})
                 continue;
-
-            if (player.IsAlive)
+            
+            if (!ServerSpecificSettings.GetAutoFillEnabled(player.ReferenceHub))
                 continue;
-
-            if (player == null)
-                continue;
-
-            ReferenceHub listHub = player.ReferenceHub;
-
-            if (listHub == null)
-                continue;
-
-            if (ServerSpecificSettings.savedSettings[listHub].Item2 != true)
-                continue;
-
+            
             ev.Player.SendBroadcast($"<size=36>[ZombieOptOut] <b>{ev.Target.DisplayName}</b> Opted out of being revived and has been replaced with <b>{player.DisplayName}</b></size>", 5);
-            RoleFill(player);
+            _optedOutZombies.Remove(ev.Target.UserId);
+            
+            if (!_positionsToReplace.TryGetValue(ev.Target.UserId, out var position))
+                position = GetValidZombiePosition(ev.Ragdoll.Position);
+            
+            _positionsToReplace[player.UserId] = position;
+            ev.Target = player;
+            ev.IsAllowed = true;
             return;
         }
 
         CL.Info("No players had Auto-Fill enabled!");
-        ClampedCompensation();
+        ev.Player.SendBroadcast($"<size=36>[ZombieOptOut] {ev.Target.DisplayName} has opted-out of being a zombie!</size>", 5);
+        
+        BroadcastUserOptedOut(optedOutPlayer);
+        
+        Timing.CallDelayed(Main.Instance.Config?.ZombieFillDuration ?? Defaults.ZombieFillDuration, () =>
+        {
+            if (!NeedReplacement) return;
+            if (!Player.TryGet(_latest049Player, out var player) || player.Role != RoleTypeId.Scp049) return;
+            
+            float compensation = 0;
 
-        foreach (Player player in Player.ReadyList)
+            for (var i = 0; i < _optedOutZombies.Count; i++)
+                compensation += Main.Instance.Config?.HealthCompensation ?? Defaults.HealthCompensation;
+
+            compensation = (Main.Instance.Config?.StackZombieCompensation ?? Defaults.StackZombieCompensation) ? compensation : Main.Instance.Config?.HealthCompensation ?? Defaults.HealthCompensation;
+            player.Heal(compensation);
+            player.SendBroadcast($"<size=36>[ZombieOptOut] You were compensated for a zombie opting out with <b>+{compensation} HP</b></size>", 5);
+            _optedOutZombies.Clear();
+        });
+    }
+
+    private static void BroadcastUserOptedOut(Player optedOutPlayer)
+    {
+        foreach (var player in Player.ReadyList.ToArray())
         {
             if (player == optedOutPlayer)
                 continue;
@@ -78,53 +106,77 @@ public class OptOutSystem
             if (player.IsDummy)
                 continue;
 
-            player.SendBroadcast($"<size=40>[ZombieOptOut] <b>{ev.Target.DisplayName}</b> Has opted out of being a zombie, you can take their spot!\n</size><size=34>By typing <b>.optin</b> in your console (`)!</size>", 5);
+            player.SendBroadcast($"<size=40>[ZombieOptOut] <b>{optedOutPlayer.DisplayName}</b> Has opted out of being a zombie, you can take their spot!\n" +
+                                 $"</size><size=34>By typing <b>.optin</b> in your console (`)!</size>", 5);
         }
-
-        Timing.CallDelayed(ZombieOptOut.Main.Instance.Config.ZombieFillDuration, () =>
-        {
-            if (waitingForCompensationFrom != 0)
-            {
-                foreach (Player player in Player.ReadyList)
-                {
-                    if (player.Role != RoleTypeId.Scp049)
-                        continue;
-
-                    player.Heal(ZombieOptOut.Main.Instance.Config.HealthCompensation);
-                    player.SendBroadcast($"<size=36>[ZombieOptOut] You were compenstaed for a zombie opting out with <b>+{ZombieOptOut.Main.Instance.Config.HealthCompensation}HP</b></size>", 5);
-                }
-
-                ClampedCompensation(-1);
-            }
-        });
     }
 
-    public static void RoleFill(Player player)
+    public static void RevivedZombie(Scp049ResurrectedBodyEventArgs ev)
     {
+        if (!_positionsToReplace.TryGetValue(ev.Target.UserId, out var position))
+            return;
+        
+        ev.Target.Position = position;
+        _positionsToReplace.Remove(ev.Target.UserId);
+    }
+
+    public static void RoleFill(Player player, string? replacedId = null)
+    {
+        if (!NeedReplacement)
+        {
+            CL.Info("No zombie roles waiting to be filled!");
+            return;
+        }
+        
         CL.Info($"Player {player.DisplayName} Auto-Filled!");
         player.SetRole(RoleTypeId.Scp0492, RoleChangeReason.Revived);
-        Timing.CallDelayed(0.5f, () => player.Position = main049Player.Position);
-        ClampedCompensation(-1);
-
-        Timing.CallDelayed(1.5f, () =>
+        player.Position = GetReadyPosition() ?? GetValidZombiePosition(defaultPosition:player.Position);
+        
+        replacedId ??= _optedOutZombies.FirstOrDefault();
+        if (string.IsNullOrEmpty(replacedId))
         {
-            if (savedCustomRole == null)
-                return;
+            CL.Info("No zombie roles waiting to be filled!");
+            return;
+        }
+        
+        var xpToGive = Main.Instance.Config?.OptInExp ?? Defaults.OptedInExp;
+        var modifier = xpToGive > 0 ? "+" : "-";
+        if (xpToGive != 0)
+            XPSystem.BackEnd.XpSystemAPI.AddXP(player, xpToGive, $"Opted-in as a Zombie [{modifier}{xpToGive}]");
+        
+        _optedOutZombies.Remove(replacedId);
+    }
 
-            Server.RunCommand($"/scr set {savedCustomRole.Rolename} {player.PlayerId}");
-            savedCustomRole = null;
-        });
-
-        XPSystem.BackEnd.XpSystemAPI.AddXP(player, 100, "Opted-in as a Zombie [+100]");
+    private static Vector3 GetValidZombiePosition(Vector3 defaultPosition)
+    {
+        Player? scp0492 = null;
+        Player? randomScp = null;
+        
+        foreach (var player in Player.ReadyList.ToArray())
+        {
+            if (player.Role == RoleTypeId.Scp049)
+                return player.Position;
+            
+            if (scp0492 == null && player.Role == RoleTypeId.Scp0492)
+                scp0492 = player;
+            if (randomScp == null && player.Team == Team.SCPs)
+                randomScp = player;
+        }
+        return scp0492?.Position ?? randomScp?.Position ?? defaultPosition;
     }
 
     internal static void RoundStart()
     {
-        savedCustomRole = null;
+        _optedOutZombies.Clear();
+        _latest049Player = null;
+        _positionsToReplace.Clear();
     }
 
-    private static void ClampedCompensation(int value = 1)
+    public static void CacheDeathPositions(PlayerDyingEventArgs ev)
     {
-        waitingForCompensationFrom = (uint)(Mathf.Clamp(waitingForCompensationFrom + value, 0, 50));
+        if (ev.Player.Role != RoleTypeId.Scp0492)
+            return;
+        
+        _positionsToReplace[ev.Player.UserId] = ev.Player.Position;
     }
 }
