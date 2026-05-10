e0993945ce39734d5c19b3288cce33ba7a3ab870
z5tm <onthebehalfofz5tm@gmail.com>
2026-05-10 01:36:27 -0500

    refactor(main): harden instance accessor, fix init order, manage listeners
    - Added HarmonyId as a const
    - Listener handling through a function
    - Bumped version due to major changes

--- Main.cs
+++ Main.cs
@@ -2,50 +2,69 @@
 using LabApi.Loader.Features.Plugins;
 using LabApi.Loader.Features.Plugins.Enums;
 using HarmonyLib;
+using LabApi.Events.Handlers;
 
 namespace ZombieOptOut;
 
 public class Main : Plugin<Config>
 {
-    public static Main Instance { get; private set; }
     #region Plugin Info
     public override string Author => "Kadava";
     public override string Name => "ZombieOptOut";
-    public override Version Version => new Version(0, 1);
+    public override Version Version => new(0, 2);
     public override string Description => "Allows players to opt-out of being SCP-049-2";
     public override Version RequiredApiVersion => LabApi.Features.LabApiProperties.CurrentVersion;
     public override LoadPriority Priority => LoadPriority.Lowest;
     #endregion
-
-    Harmony harmony;
+    
+    private static Main? _instance;
+    public static Main Instance => _instance ?? throw new InvalidOperationException("ZombieOptOut is not initialized!");
+    private Harmony? _harmony;
+    private const string HarmonyId = "com.kadava.zombieoptout";
 
     public override void Enable()
     {
-        harmony = new Harmony("ZombieOptOut");
-        harmony.PatchAll();
-        Instance = this;
+        _instance = this;
+        
+        _harmony = new Harmony(HarmonyId);
+        _harmony.PatchAll();
 
-        LabApi.Events.Handlers.Scp049Events.ResurrectedBody += OptOutSystem.RevivedZombie;
-        LabApi.Events.Handlers.ServerEvents.RoundStarted += OptOutSystem.RoundStart;
-        LabApi.Events.Handlers.ServerEvents.RoundStarted += AFKReplacement.OnServerRoundStarted;
-        LabApi.Events.Handlers.PlayerEvents.UpdatingEffect += AFKReplacement.OnUpdatingEffects;
+        ManageListeners();
         ServerSpecificSettings.Initialize();
     }
     public override void Disable()
     {
-        harmony.UnpatchAll(harmony.Id);
-        Instance = null;
-
-        LabApi.Events.Handlers.Scp049Events.ResurrectedBody -= OptOutSystem.RevivedZombie;
-        LabApi.Events.Handlers.ServerEvents.RoundStarted -= OptOutSystem.RoundStart;
-        LabApi.Events.Handlers.ServerEvents.RoundStarted -= AFKReplacement.OnServerRoundStarted;
-        LabApi.Events.Handlers.PlayerEvents.UpdatingEffect -= AFKReplacement.OnUpdatingEffects;
+        ManageListeners(pluginEnabled:false);
+        
+        _harmony?.UnpatchAll(HarmonyId);
+        _instance = null;
         ServerSpecificSettings.DeInitialize();
+    }
+
+    private static void ManageListeners(bool pluginEnabled = true)
+    {
+        if (pluginEnabled)
+        {
+            Scp049Events.ResurrectedBody += OptOutSystem.RevivedZombie;
+            Scp049Events.ResurrectingBody += OptOutSystem.RevivingZombie;
+            ServerEvents.RoundStarted += OptOutSystem.RoundStart;
+            ServerEvents.RoundStarted += AFKReplacement.OnServerRoundStarted;
+            PlayerEvents.UpdatingEffect += AFKReplacement.OnUpdatingEffects;
+            PlayerEvents.Dying += OptOutSystem.CacheDeathPositions;
+            return;
+        }
+        
+        Scp049Events.ResurrectedBody -= OptOutSystem.RevivedZombie;
+        Scp049Events.ResurrectingBody -= OptOutSystem.RevivingZombie;
+        ServerEvents.RoundStarted -= OptOutSystem.RoundStart;
+        ServerEvents.RoundStarted -= AFKReplacement.OnServerRoundStarted;
+        PlayerEvents.UpdatingEffect -= AFKReplacement.OnUpdatingEffects;
+        PlayerEvents.Dying -= OptOutSystem.CacheDeathPositions;
     }
 
     public override void LoadConfigs()
     {
-        if (!this.TryLoadConfig(ConfigFileName, out Config config, true))
+        if (!this.TryLoadConfig(ConfigFileName, out Config? config, true))
         {
             CL.Warn("Failed to load the configuration file, using default values.");
             config = new Config();
