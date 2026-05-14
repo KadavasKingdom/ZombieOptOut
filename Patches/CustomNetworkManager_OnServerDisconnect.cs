b9169e59fc34441262996c2c441253b5406a5383
z5tm <onthebehalfofz5tm@gmail.com>
2026-05-13 19:20:40 -0500

    chore(harmony-patch): class and file name

--- Patches/CustomNetworkManager_OnServerDisconnect.cs
+++ Patches/CustomNetworkManager_OnServerDisconnect.cs
@@ -6,7 +6,7 @@ namespace ZombieOptOut.Patches;
 // this simple patch just moves the logic for handling disconnected players to before they take damage
 // this is as to cache their health before they died
 [HarmonyPatch(typeof(CustomNetworkManager), nameof(CustomNetworkManager.OnServerDisconnect))]
-internal class AfkReplacementPatch
+internal class CustomNetworkManager_OnServerDisconnect
 {
     public static void Prefix(CustomNetworkManager __instance, NetworkConnectionToClient conn)
     {
